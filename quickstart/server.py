6be6df157eb33e50d1f56ed7d77ba56bd299878d
James Douglass <jdouglass@stanford.edu>
2022-04-01 17:06:04 -0700

    Minor tweaks for typos

--- quickstart/server.py
+++ quickstart/server.py
@@ -5,7 +5,7 @@ import falcon.asgi
 # Because Falcon follows REST, we think in terms of resources and state
 # transitions, mapping to HTTP verbs.
 class ThingsResource:
-    async def on_get(seld, request, response):
+    async def on_get(self, request, response):
         """Handles GET requests"""
         response.status = falcon.HTTP_200  # this is the default status
         response.content_type = falcon.MEDIA_TEXT  # default is JSON, so this is an override
@@ -20,5 +20,5 @@ app = falcon.asgi.App()
 # Resources are represented by long-lived class instances
 things = ThingsResource()
 
-# things will hadnle all requests to the `/things' URL path.
+# things will handle all requests to the `/things' URL path.
 app.add_route('/things', things)
